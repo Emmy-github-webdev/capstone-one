d5551b3a1c26ccb5f1217ff07750968ce0a98dea
Emmanuel Oga <ogahemmanuel75@gmail.com>
2021-10-23 12:47:51 +0100

    Add live demo link to README

--- javaScript/scripts.js
+++ javaScript/scripts.js
@@ -18,7 +18,7 @@ const featuredSpeakersDiv = document.querySelector('.featured-speaker-content');
 const featuredSpeakers = [
   {
     id: 1,
-    speakerImage: '/images/sridhar.jpg',
+    speakerImage: 'images/sridhar.jpg',
     speakerName: 'Udbhav Tiwari',
     speakerTitle: 'Minister of Communication',
     speakerDetail: 'Udbhav Tiwari is the minister of communication of Federal Republic of Nigeria.',
@@ -26,7 +26,7 @@ const featuredSpeakers = [
 
   {
     id: 2,
-    speakerImage: '/images/subhas.jpg',
+    speakerImage: 'images/subhas.jpg',
     speakerName: 'Vidur Gupta',
     speakerTitle: 'Founder & CMO, Indusface',
     speakerDetail: 'Vidur Gupta is the founder and CMO of Indusface security.',
@@ -34,7 +34,7 @@ const featuredSpeakers = [
 
   {
     id: 3,
-    speakerImage: '/images/subhashish.jpg',
+    speakerImage: 'images/subhashish.jpg',
     speakerName: 'James Oche',
     speakerTitle: 'President, Nigeria SME Forum',
     speakerDetail: 'James Oche is the President, Nigeria SME forum Nigeria.',
@@ -42,7 +42,7 @@ const featuredSpeakers = [
 
   {
     id: 4,
-    speakerImage: '/images/sunil.jpg',
+    speakerImage: 'images/sunil.jpg',
     speakerName: 'Visagan Subburayalu',
     speakerTitle: 'Director of Technology Target',
     speakerDetail: '  Visagan Subburayalu is the Director of Technology Target limited.',
@@ -50,7 +50,7 @@ const featuredSpeakers = [
 
   {
     id: 5,
-    speakerImage: '/images/udbhav.jpg',
+    speakerImage: 'images/udbhav.jpg',
     speakerName: 'Dr Suleman Lamido',
     speakerTitle: ' Kaduna state commissioner for communication',
     speakerDetail: 'Dr Suleman Lamido is the commissioner for communication of Kaduna state.',
@@ -58,7 +58,7 @@ const featuredSpeakers = [
 
   {
     id: 6,
-    speakerImage: '/images/attahiru.jpeg',
+    speakerImage: 'images/attahiru.jpeg',
     speakerName: 'MJ Attahiru Sule',
     speakerTitle: 'Major General of Nigerian Army',
     speakerDetail: 'MJ Attahiru Sule is the Cheif of Defence Staff of Nigeria.',
